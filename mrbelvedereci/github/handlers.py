8250b7da18af1576f79b29c470d9fb95dda13a3c
Jason Lantz <jlantz@salesforce.com>
2016-11-23 15:37:52 -0600

    fix import

--- mrbelvedereci/github/handlers.py
+++ mrbelvedereci/github/handlers.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from github3 import login
 from mrbelvedereci.github.models import Repository
