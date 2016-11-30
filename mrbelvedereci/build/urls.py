8fa038b3542086e84dc16e27e4093f11869128ea
Jason Lantz <jlantz@salesforce.com>
2016-11-30 08:29:23 -0600

    Added build rerun view

--- mrbelvedereci/build/urls.py
+++ mrbelvedereci/build/urls.py
@@ -3,5 +3,6 @@ from mrbelvedereci.build import views
 
 urlpatterns = [
     url(r'^$', views.build_list),
+    url(r'^/(?P<build_id>\w+)/rerun$', views.build_rerun),
     url(r'^/(?P<build_id>\w+)$', views.build_detail),
 ]
--- mrbelvedereci/build/views.py
+++ mrbelvedereci/build/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 from ansi2html import Ansi2HTMLConverter
 
 from mrbelvedereci.build.models import Build
+from mrbelvedereci.build.tasks import run_build
 
 def build_list(request):
     pass
@@ -12,3 +13,10 @@ def build_detail(request, build_id):
     build = get_object_or_404(Build, id = build_id)
     
     return render(request, 'build/build_detail.html', context={'build': build})
+
+def build_rebuild(request, build_id):
+    build = get_object_or_404(Build, id = build_id)
+
+    run_build.apply_async((build.id,), countdown=1)
+   
+    return HttpResponseRedirect('/builds/{}'.format(build.id)) 
