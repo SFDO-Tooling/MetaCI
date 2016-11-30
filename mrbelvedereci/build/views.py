1ffa99257f6dcac30a6d61a94bde3f73ce3957a4
Jason Lantz <jlantz@salesforce.com>
2016-11-30 08:43:50 -0600

    fix import

--- mrbelvedereci/build/views.py
+++ mrbelvedereci/build/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from ansi2html import Ansi2HTMLConverter
 
