55ab8b7724196d33f301a6b78e97876cce6e348e
Jason Lantz <jlantz@salesforce.com>
2016-11-30 08:30:52 -0600

    fix view name

--- mrbelvedereci/build/urls.py
+++ mrbelvedereci/build/urls.py
@@ -3,6 +3,6 @@ from mrbelvedereci.build import views
 
 urlpatterns = [
     url(r'^$', views.build_list),
-    url(r'^/(?P<build_id>\w+)/rerun$', views.build_rerun),
+    url(r'^/(?P<build_id>\w+)/rebuild$', views.build_rebuild),
     url(r'^/(?P<build_id>\w+)$', views.build_detail),
 ]
