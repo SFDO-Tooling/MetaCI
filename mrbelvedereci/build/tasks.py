07852d18ed3a1bbb27baa7bf403a912325d01da2
Jason Lantz <jlantz@salesforce.com>
2016-12-14 08:22:37 -0600

    Remove call to nonexistent logger

--- mrbelvedereci/build/tasks.py
+++ mrbelvedereci/build/tasks.py
@@ -42,7 +42,6 @@ def check_queued_build(build_id):
     # If this is not a scratch org, ensure no builds are currently running against the org
     if not org.scratch:
         lock_id = 'mrbelvedereci-org-lock-{}'.format(org.id)
-        logger.debug('Locking org: %s', org)
         with memcache_lock(lock_id, self.app.oid) as acquired:
             if acquired:
                 # Run the build
