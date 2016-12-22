ce51aefb236b40fd51bdeaf64a3f8a7e911b4ca2
Jason Lantz <jlantz@salesforce.com>
2016-12-22 15:47:00 -0600

    Part 2 of porting ApexTestsDB

---
+++ mrbelvedereci/testresults/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from mrbelvedereci.testresults import views
+
+urlpatterns = [
+    url(r'^/(?P<build_id>\d+)/(?P<flow>.*)$', views.build_flow_tests, name="build_flow_tests"),
+]
