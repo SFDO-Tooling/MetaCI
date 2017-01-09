73e42cc3523a6f111486cea26413d79ec9f5da6d
Jason Lantz <jlantz@salesforce.com>
2017-01-09 11:05:17 -0600

    Add filters to trigger view in admin

--- mrbelvedereci/trigger/admin.py
+++ mrbelvedereci/trigger/admin.py
@@ -3,4 +3,5 @@ from mrbelvedereci.trigger.models import Trigger
 
 class TriggerAdmin(admin.ModelAdmin):
     list_display = ('repo', 'type', 'flows', 'org', 'regex', 'active', 'public')
+    list_filter = ('active', 'public', 'repo', 'org', 'type')
 admin.site.register(Trigger, TriggerAdmin)
