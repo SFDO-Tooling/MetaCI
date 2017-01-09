bb3d9ec2d9932da2abb50f5cb6bceffae5112abb
Jason Lantz <jlantz@salesforce.com>
2017-01-09 11:37:12 -0600

    Add name to trigger list view

--- mrbelvedereci/trigger/admin.py
+++ mrbelvedereci/trigger/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from mrbelvedereci.trigger.models import Trigger
 
 class TriggerAdmin(admin.ModelAdmin):
-    list_display = ('repo', 'type', 'flows', 'org', 'regex', 'active', 'public')
-    list_filter = ('active', 'public', 'repo', 'org', 'type')
+    list_display = ('name', 'repo', 'type', 'flows', 'org', 'regex', 'active', 'public')
+    list_filter = ('active', 'public', 'type', 'org', 'repo')
 admin.site.register(Trigger, TriggerAdmin)
