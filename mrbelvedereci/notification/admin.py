b9bfeac9cd71954cbadc9c6ea4128ea15f6d9079
Jason Lantz <jlantz@salesforce.com>
2017-01-12 09:38:45 -0600

    Add admin mappings for notification models

--- mrbelvedereci/notification/admin.py
+++ mrbelvedereci/notification/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from mrbelvedereci.notification.models import RepositoryNotification
+from mrbelvedereci.notification.models import BranchNotification
+from mrbelvedereci.notification.models import PlanNotification
 
-# Register your models here.
+class RepositoryNotificationAdmin(admin.ModelAdmin):
+    list_display = ('repo', 'user')
+    list_filter = ('repo', 'user')
+admin.site.register(RepositoryNotification, RepositoryNotificationAdmin)
+
+class BranchNotificationAdmin(admin.ModelAdmin):
+    list_display = ('branch', 'user')
+    list_filter = ('branch__repo', 'user')
+admin.site.register(BranchNotification, BranchNotificationAdmin)
+
+class PlanNotificationAdmin(admin.ModelAdmin):
+    list_display = ('plan', 'user')
+    list_filter = ('plan__repo', 'user')
+admin.site.register(PlanNotification, PlanNotificationAdmin)
