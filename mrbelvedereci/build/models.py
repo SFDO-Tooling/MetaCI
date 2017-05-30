f2337a5a602a13d59c6f0e3aadcf5aa138fb601e
Jason Lantz <jlantz@salesforce.com>
2017-05-30 18:45:07 -0500

    Emit a warning message in the log if no test results found when configured with a junit_path

--- mrbelvedereci/build/models.py
+++ mrbelvedereci/build/models.py
@@ -355,9 +355,11 @@ class BuildFlow(models.Model):
     def load_test_results(self):
         results = []
         if self.build.plan.junit_path:
-            if os.path.exists(self.build.plan.junit_path):
-                for filename in iglob(self.build.plan.junit_path):
-                    results.extend(self.load_junit(filename))
+            for filename in iglob(self.build.plan.junit_path):
+                results.extend(self.load_junit(filename))
+            if not results:
+                self.logger.warning('JUnit path {} not found'.format(self.build.plan.junit_path))
+                return
         else:
             try:
                 with open('test_results.json', 'r') as f:
