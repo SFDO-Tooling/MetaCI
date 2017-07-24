fdacfc77cfb81f7f45429c59e4cee61e844b30e2
Chris Polcyn <cpolcyn@salesforce.com>
2017-07-24 15:28:10 -0700

    fix builds/search URL

--- mrbelvedereci/build/urls.py
+++ mrbelvedereci/build/urls.py
@@ -3,7 +3,7 @@ from mrbelvedereci.build import views
 
 urlpatterns = [
     url(r'^$', views.build_list, name="home"),
-    url(r'^search$', views.build_search, name="build_search"),
+    url(r'^/search$', views.build_search, name="build_search"),
     url(r'^/(?P<build_id>\w+)/rebuild$', views.build_rebuild, name="build_rebuild"),
     url(r'^/(?P<build_id>\w+)/rebuilds/(?P<rebuild_id>\w+)/(?P<tab>\w+)$', views.build_detail, name="build_rebuild_detail_tab"),
     url(r'^/(?P<build_id>\w+)/rebuilds/(?P<rebuild_id>\w+)$', views.build_detail, name="build_rebuild_detail"),
