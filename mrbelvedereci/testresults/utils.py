4d0a66c529694c8e48c4955291a9b21a9bc5895f
Christian Carter <christian.carter@salesforce.com>
2017-08-14 16:31:23 -0700

    raise forbidden for private plans

--- mrbelvedereci/testresults/utils.py
+++ mrbelvedereci/testresults/utils.py
@@ -1,3 +1,4 @@
+from django.http import Http403
 from django.shortcuts import get_object_or_404
 from mrbelvedereci.build.models import Build
 from mrbelvedereci.build.models import BuildFlow
@@ -8,7 +9,7 @@ def find_buildflow(request, build_id, flow):
     build = get_object_or_404(Build, id=build_id)
 
     if not build.plan.public and not request.user.is_staff:
-        raise Http404()
+        raise Http403()
     query = {'build_id': build_id, 'flow': flow}
     if build.current_rebuild:
         query['rebuild_id'] = build.current_rebuild
