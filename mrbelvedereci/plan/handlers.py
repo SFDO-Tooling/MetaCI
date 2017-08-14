4d5f7365807c36fc2f8963d08680641b63047816
Christian Carter <christian.carter@salesforce.com>
2017-08-14 14:34:08 -0700

    remove logentry

--- mrbelvedereci/plan/handlers.py
+++ mrbelvedereci/plan/handlers.py
@@ -2,31 +2,8 @@ import requests
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.admin.models import LogEntry, ADDITION
-from django.contrib.contenttypes.models import ContentType
 from github3 import login
 from mrbelvedereci.plan.models import Plan
-from mrbelvedereci.build.models import Build
-
-
-@receiver(post_save, sender=Build)
-def log_manual_build(sender, **kwargs):
-    # skip updates
-    if not kwargs['created']:
-        return
-
-    build = kwargs['instance']
-
-    # Log the new build and who created it.
-    LogEntry.objects.log_action(
-            user_id = build.user.id,
-            content_type_id = ContentType.objects.get_for_model(build).id,
-            object_id = build.id,
-            object_repr = build.__str__(),
-            change_message = 'started a build',
-            action_flag = ADDITION,
-    )
-
 
 @receiver(post_save, sender=Plan)
 def create_plan_webhooks(sender, **kwargs):
