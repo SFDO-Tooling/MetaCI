8b839cb027467e4ce198b5fa6f445f3d5aebbc11
Christian Carter <christian.carter@salesforce.com>
2017-08-15 16:00:57 -0700

    add branch search

--- mrbelvedereci/testresults/filters.py
+++ mrbelvedereci/testresults/filters.py
@@ -4,8 +4,10 @@ from mrbelvedereci.build.models import BuildFlow
 
 class BuildFlowFilter(django_filters.FilterSet):
     plan = django_filters.CharFilter(name="build__plan__name", label='Plan Name', lookup_expr='contains')
+    branch = django_filters.CharFilter(name="build__branch__name", label='Branch Name', lookup_expr='contains')
     build = django_filters.CharFilter(name='build')
+    start_date = django_filters.DateFromToRangeFilter(name="build__time_start", label="Date")
 
     class Meta:
         model = BuildFlow
-        fields = ['build','plan']
+        fields = ['build','plan','branch']
