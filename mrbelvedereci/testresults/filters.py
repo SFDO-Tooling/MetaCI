86620b6d0a6f64a0f42f0d8d267a1709b5e732dc
Christian Carter <christian.carter@salesforce.com>
2017-08-16 11:56:56 -0700

    better filter experience

--- mrbelvedereci/testresults/filters.py
+++ mrbelvedereci/testresults/filters.py
@@ -7,7 +7,7 @@ class BuildFlowFilter(django_filters.FilterSet):
     plan = django_filters.CharFilter(name="build__plan__name", label='Plan Name', lookup_expr='contains')
     branch = django_filters.CharFilter(name="build__branch__name", label='Branch Name', lookup_expr='contains')
     build = django_filters.CharFilter(name='build')
-    start_date = django_filters.DateFromToRangeFilter(name="build__time_start", label="Date",widget=RangeWidget(attrs={'placeholder': 'MM/DD/YYYY'}))
+    start_date = django_filters.DateFromToRangeFilter(name="build__time_start", label="Start Date",widget=RangeWidget(attrs={'placeholder': 'MM/DD/YYYY'}))
 
     class Meta:
         model = BuildFlow
