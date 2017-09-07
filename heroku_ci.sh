6678170b8346f328f3d8dd006bbbefeb4201b916
Jason Lantz <jlantz@salesforce.com>
2017-09-07 09:07:06 -0500

    Fix clone command

--- heroku_ci.sh
+++ heroku_ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script runs the tests on Heroku CI
 
-git clone -b "$HEROKU_TEST_RUN_BRANCH" --single-branch https://github.com/SalesforceFoundation/mrbelvedereci_checkout 
+git clone -b "$HEROKU_TEST_RUN_BRANCH" --single-branch https://github.com/SalesforceFoundation/mrbelvedereci mrbelvedereci_checkout 
 cd mrbelvedereci_checkout
 git reset --hard $HEROKU_TEST_RUN_COMMIT_VERSION
 export DJANGO_SETTINGS_MODULE=config.settings.test
