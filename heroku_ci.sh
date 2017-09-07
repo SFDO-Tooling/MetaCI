d51268dad80c64dc57c8f1006c57aa9406028d17
Jason Lantz <jlantz@salesforce.com>
2017-09-07 09:03:43 -0500

    Uncomment lines

--- heroku_ci.sh
+++ heroku_ci.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # This script runs the tests on Heroku CI
 
-#git clone -b "$HEROKU_TEST_RUN_BRANCH" --single-branch https://github.com/SalesforceFoundation/mrbelvedereci_checkout 
-#cd mrbelvedereci_checkout
-#git reset --hard $HEROKU_TEST_RUN_COMMIT_VERSION
-coverage run --source=mrbelvedereci manage.py test
-#nosetests --with-tap --tap-stream --with-coverage --cover-package=cumulusci
-#coveralls
+git clone -b "$HEROKU_TEST_RUN_BRANCH" --single-branch https://github.com/SalesforceFoundation/mrbelvedereci_checkout 
+cd mrbelvedereci_checkout
+git reset --hard $HEROKU_TEST_RUN_COMMIT_VERSION
+export DJANGO_SETTINGS_MODULE=config.settings.test
+python manage.py test
+coveralls
