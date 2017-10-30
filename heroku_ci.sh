2fc1bd7ac49aeec4262ab27736dbef501159996e
Jason Lantz <jlantz@salesforce.com>
2017-10-30 16:39:15 -0500

    Throw non-zero exit status if tests fail

--- heroku_ci.sh
+++ heroku_ci.sh
@@ -6,4 +6,8 @@ cd MetaCI_checkout
 git reset --hard $HEROKU_TEST_RUN_COMMIT_VERSION
 export DJANGO_SETTINGS_MODULE=config.settings.test
 python manage.py test
+exit_status=$?
 coveralls
+if [ "$exit_status" != "0" ]; then
+    exit $exit_status
+fi
