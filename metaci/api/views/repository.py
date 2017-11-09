8b1049bea43fe6f5f85c616267d27406b6eda32f
Christian Carter <christian.carter@salesforce.com>
2017-11-09 11:50:43 -0800

    pkorslug for repo

--- metaci/api/views/repository.py
+++ metaci/api/views/repository.py
@@ -1,24 +1,25 @@
-from django.shortcuts import render
 from metaci.api.serializers.repository import BranchSerializer
 from metaci.api.serializers.repository import RepositorySerializer
 from metaci.repository.filters import BranchFilter
 from metaci.repository.filters import RepositoryFilter
 from metaci.repository.models import Branch
 from metaci.repository.models import Repository
+from metaci.api.utils import PkOrSlugMixin
 from rest_framework import viewsets
 
 class BranchViewSet(viewsets.ModelViewSet):
     """
-    A viewset for viewing and editing user instances.
+    A viewset for viewing and editing Branch instances.
     """
     serializer_class = BranchSerializer
     queryset = Branch.objects.all()
     filter_class = BranchFilter
 
-class RepositoryViewSet(viewsets.ModelViewSet):
+class RepositoryViewSet(PkOrSlugMixin, viewsets.ModelViewSet):
     """
-    A viewset for viewing and editing user instances.
+    A viewset for viewing and editing Repo instances.
     """
     serializer_class = RepositorySerializer
     queryset = Repository.objects.all()
     filter_class = RepositoryFilter
+    lookup_slug_field = 'name'
