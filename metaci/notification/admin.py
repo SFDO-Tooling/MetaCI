dfed1756ac5f3253e99acd3a253bba3d85200465
Chris Polcyn <cpolcyn@salesforce.com>
2018-03-13 16:46:35 -0700

    remove PlanRepository notifications

--- metaci/notification/admin.py
+++ metaci/notification/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from metaci.notification.models import RepositoryNotification
 from metaci.notification.models import BranchNotification
 from metaci.notification.models import PlanNotification
-from metaci.notification.models import PlanRepositoryNotification
 
 class RepositoryNotificationAdmin(admin.ModelAdmin):
     list_display = ('repo', 'user')
@@ -18,8 +17,3 @@ class PlanNotificationAdmin(admin.ModelAdmin):
     list_display = ('plan', 'user')
     list_filter = ('plan__repos', 'user')
 admin.site.register(PlanNotification, PlanNotificationAdmin)
-
-class PlanRepositoryNotificationAdmin(admin.ModelAdmin):
-    list_display = ('planrepository', 'user')
-    list_filter = ('planrepository__repo', 'user')
-admin.site.register(PlanRepositoryNotification, PlanRepositoryNotificationAdmin)
