1df3cadbe93579c461c88dd09b02b78aff864669
Christian Carter <christian.carter@salesforce.com>
2018-05-31 15:09:49 -0700

    uncomment

--- metaci/views.py
+++ metaci/views.py
@@ -6,7 +6,7 @@ import sys
 import django
 from django.conf import settings
 from django.views.generic.base import TemplateView
-#from pip.utils import get_installed_version
+from pip.utils import get_installed_version
 
 
 class AboutView(TemplateView):
@@ -35,7 +35,7 @@ class AboutView(TemplateView):
             context['SFDX_CLI_VERSION'] = match.group(1)
 
         # cumulusci
-        #context['CUMULUSCI_VERSION'] = get_installed_version('cumulusci')
+        context['CUMULUSCI_VERSION'] = get_installed_version('cumulusci')
 
         # heroku
         heroku_env_vars = [
