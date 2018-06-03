2649ee7e8f7b608b8ee74fc37bf38a6f4faf803f
Christian Carter <christian.carter@salesforce.com>
2018-06-03 15:02:36 -0700

    add follow bool

--- metaci/notification/models.py
+++ metaci/notification/models.py
@@ -2,31 +2,28 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
-class PlanNotification(models.Model):
+
+class AbstractNotification(models.Model):
+    on_success = models.BooleanField(default=False)
+    on_fail = models.BooleanField(default=True)
+    on_error = models.BooleanField(default=True)
+    follow = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+class PlanNotification(AbstractNotification):
     user = models.ForeignKey('users.User', related_name='plan_notifications', on_delete=models.CASCADE)
     plan = models.ForeignKey('plan.Plan', related_name='notifications', on_delete=models.CASCADE)
-    on_success = models.BooleanField(default=False)
-    on_fail = models.BooleanField(default=True)
-    on_error = models.BooleanField(default=True)
 
-class PlanRepositoryNotification(models.Model):
+class PlanRepositoryNotification(AbstractNotification):
     user = models.ForeignKey('users.User', related_name='planrepository_notifications')
     planrepository = models.ForeignKey('plan.PlanRepository', related_name='notifications')
-    on_success = models.BooleanField(default=False)
-    on_fail = models.BooleanField(default=True)
-    on_error = models.BooleanField(default=True)
 
-class BranchNotification(models.Model):
+class BranchNotification(AbstractNotification):
     user = models.ForeignKey('users.User', related_name='branch_notifications', on_delete=models.CASCADE)
     branch = models.ForeignKey('repository.Branch', related_name='notifications', on_delete=models.CASCADE)
-    on_success = models.BooleanField(default=False)
-    on_fail = models.BooleanField(default=True)
-    on_error = models.BooleanField(default=True)
 
-class RepositoryNotification(models.Model):
+class RepositoryNotification(AbstractNotification):
     user = models.ForeignKey('users.User', related_name='repo_notifications', on_delete=models.CASCADE)
     repo = models.ForeignKey('repository.Repository', related_name='notifications', on_delete=models.CASCADE)
-    on_success = models.BooleanField(default=False)
-    on_fail = models.BooleanField(default=True)
-    on_error = models.BooleanField(default=True)
