c549ad25f4c3d05e3be36464eb621d18724a712e
David Glick <dglick@salesforce.com>
2018-11-21 13:50:40 -0500

    Update for github3.py 1.x

--- metaci/release/utils.py
+++ metaci/release/utils.py
@@ -21,32 +21,31 @@ def update_release_from_github(release, repo_api=None):
             'Cannot update release, ref tags/{} not found in Github'.format(release.git_tag)
         )
         return
-    url = repo_api._build_url('releases/tags/{}'.format(release.git_tag), base_url=repo_api._api)
-    gh_release = repo_api._get(url).json()
+    gh_release = repo_api.release_by_tag_name(release.git_tag)
     release.status = 'published'
-    release.version_name = gh_release['name']
-    release.version_number = gh_release['name']
-    release.github_release = gh_release['html_url']
-    release.release_creation_date = parse_datetime(gh_release['created_at'])
+    release.version_name = gh_release.name
+    release.version_number = gh_release.name
+    release.github_release = gh_release.html_url
+    release.release_creation_date = gh_release.created_at
     release.created_from_commit = ref.object.sha
     sandbox_date = re.findall(
-        r'^Sandbox orgs: (20[\d][\d]-[\d][\d]-[\d][\d])', gh_release['body']
+        r'^Sandbox orgs: (20[\d][\d]-[\d][\d]-[\d][\d])', gh_release.body
     )
     if sandbox_date:
         release.sandbox_push_date = parse_date(sandbox_date[0], '%Y-%m-%d')
 
     prod_date = re.findall(
-        r'^Production orgs: (20[\d][\d]-[\d][\d]-[\d][\d])', gh_release['body']
+        r'^Production orgs: (20[\d][\d]-[\d][\d]-[\d][\d])', gh_release.body
     )
     if prod_date:
         release.production_push_date = parse_date(prod_date[0], '%Y-%m-%d')
 
-    package_version_id = re.findall(r'(04t[\w]{15,18})', gh_release['body'])
+    package_version_id = re.findall(r'(04t[\w]{15,18})', gh_release.body)
     if package_version_id:
         release.package_version_id = package_version_id[0]
 
     trialforce_id = re.findall(
-        r'^(0TT[\w]{15,18})', gh_release['body']
+        r'^(0TT[\w]{15,18})', gh_release.body
     )
     if trialforce_id:
         release.trialforce_id = trialforce_id[0]
