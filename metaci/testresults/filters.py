5d1772deb655ed33889d9594290cfc25698ff3c5
Paul Prescod <pprescod@salesforce.com>
2019-03-01 21:10:37 -0800

    Django filter renamed the name= parameter to field_name= .
    https://django-filter.readthedocs.io/en/master/guide/migration.html#filter-name-renamed-to-filter-field-name-792
    
    Fixes crashes on pages like this:
    
    https://mrbelvedereci.herokuapp.com/tests/60297/ci_feature/compare-to

--- metaci/testresults/filters.py
+++ metaci/testresults/filters.py
@@ -6,14 +6,14 @@ from metaci.build.models import BuildFlow
 
 class BuildFlowFilter(django_filters.FilterSet):
     plan = django_filters.CharFilter(
-        name="build__plan__name", label="Plan Name", lookup_expr="contains"
+        field_name="build__plan__name", label="Plan Name", lookup_expr="contains"
     )
     branch = django_filters.CharFilter(
-        name="build__branch__name", label="Branch Name", lookup_expr="contains"
+        field_name="build__branch__name", label="Branch Name", lookup_expr="contains"
     )
-    build = django_filters.CharFilter(name="build")
+    build = django_filters.CharFilter(field_name="build")
     start_date = django_filters.DateFromToRangeFilter(
-        name="build__time_start",
+        field_name="build__time_start",
         label="Start Date",
         widget=RangeWidget(attrs={"placeholder": "MM/DD/YYYY"}),
     )
