265d4374f7922194db48ca000d1df2f80ae0d2e5
Paul Prescod <pprescod@salesforce.com>
2019-03-04 21:05:31 -0800

    WIP - before adding date filtering

--- metaci/api/serializers/testmethod_perf.py
+++ metaci/api/serializers/testmethod_perf.py
@@ -1,13 +1,11 @@
 from rest_framework import serializers
 
 
-
 class TestMethodPerfSerializer(serializers.Serializer):
     method_name = serializers.CharField()
     count = serializers.IntegerField()
     avg = serializers.FloatField()
     repo = serializers.CharField()
-    plan = serializers.CharField()
-    flow = serializers.CharField()
-    class Meta:
-        fields = ("method_name", "count", "avg", "repo")
+#    plan = serializers.CharField()
+#    flow = serializers.CharField()
+#    branch = serializers.CharField()
