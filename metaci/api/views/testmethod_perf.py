234aa9e7387d125504b6f633b6cb548e7ed32b4d
Paul Prescod <pprescod@salesforce.com>
2019-03-04 21:05:31 -0800

    WIP - before adding date filtering

--- metaci/api/views/testmethod_perf.py
+++ metaci/api/views/testmethod_perf.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.db.models import F, Avg, Count
+from django.db import models
+from django.forms import SelectDateWidget
 
 import django_filters.rest_framework
 
@@ -12,33 +14,43 @@ from metaci.api.serializers.testmethod_perf import TestMethodPerfSerializer
 from metaci.repository.models import Repository
 from metaci.plan.models import Plan
 
+
 class TestMethodPerfFilter(django_filters.rest_framework.FilterSet):
+    def __init__(self, *args, **kwargs):
+        print("***** INITIALIZXED ***", args, kwargs)
+        super(TestMethodPerfFilter, self).__init__(*args, **kwargs)
+
     method_name = django_filters.rest_framework.CharFilter(field_name="method_name",
         label="Method Name")
-    #
-    # Doesn't work because it seems to be calling __str__ for some reason
-    # 
-    # repo = django_filters.rest_framework.ModelChoiceFilter(field_name="repo",
-    #     label="Repo Name", queryset = Repository.objects.all(),
-    #     to_field_name="name")
 
-    repo = django_filters.rest_framework.ChoiceFilter(field_name="repo",
-        label="Repo Name", choices = [(r.name, r.name) for r in Repository.objects.all()],
-        required = True)
-
-    plan = django_filters.rest_framework.ChoiceFilter(field_name="plan",
-        label="Plan Name", choices = set([(p.name, p.name) for p in Plan.objects.all()]))
-
-    flow = django_filters.rest_framework.ChoiceFilter(field_name="flow",
-        label="Flow Name", choices = set([(f.flow, f.flow) for f in BuildFlow.objects.all()]))
-
+    # we implement many of these "by hand" in get_queryset, so we don't want them
+    # here too. They should be implemented in the view because they filter the
+    # buildflow list, not the output.
     dummy_filter = lambda queryset, name, value:  queryset
 
-    foobar = django_filters.rest_framework.CharFilter(field_name="xyzzy",
-        label="Date range (unimplemented)", method = dummy_filter)
+    repo_choices = Repository.objects.values_list('name', 'name').order_by('name').distinct()
+    repo = django_filters.rest_framework.ChoiceFilter(field_name="repo", label="Repo Name", 
+         choices = repo_choices, method = dummy_filter)
+
+    plan_choices = Plan.objects.values_list('name', 'name').order_by('name').distinct()
+    plan = django_filters.rest_framework.ChoiceFilter(field_name="plan", label="Plan Name", 
+         choices = plan_choices, method = dummy_filter)
+
+    flow_choices = BuildFlow.objects.values_list('flow', 'flow').order_by('flow').distinct()
+    flow = django_filters.rest_framework.ChoiceFilter(field_name="flow",
+         label="Flow Name", choices = flow_choices,
+         method = dummy_filter)
+
+    foobar = django_filters.rest_framework.DateRangeFilter(field_name="xyzzy",
+        label="Date range (unimplemented)", method = dummy_filter,
+        )
+
+    # foobar2 = django_filters.rest_framework.DateFromToRangeFilter(field_name="xyzzy",
+    #     label="Date range (unimplemented)", method = dummy_filter,
+    #     widget = SelectDateWidget)
+
 
     o = django_filters.rest_framework.OrderingFilter(
-        # tuple-mapping retains order
         fields=(
             ('method_name', 'method_name'),
             ('avg', 'avg'),
@@ -75,15 +87,28 @@ class TestMethodPerfListView(generics.ListAPIView):
         buildflows = (BuildFlow.objects
                 .filter(time_end__isnull = False)
                 .filter(time_end__gt = range_start)
-                .filter(time_end__lt = range_end))[0:build_flows_limit]
+                .filter(time_end__lt = range_end))
+        get = self.request.query_params.get
+
+        param_filters = (("repo", "build__repo__name"), 
+                    ("plan", "build__plan__name"), 
+                    ("branch", "build__branch__name"),
+                    ("flow", "flow"))
+
+        for param, filtername in param_filters:
+            if get(param): 
+                buildflows = buildflows.filter(**{filtername: get(param)})
+                
+        buildflows = buildflows[0:build_flows_limit]
+        
         print(str(buildflows.query))
 
         queryset = TestResult.objects.filter(
                 build_flow_id__in = buildflows)\
                         .values(method_name = F('method__name'), 
                                 repo = F('build_flow__build__repo__name'),
-                                plan = F('build_flow__build__plan__name'),
-                                flow = F('build_flow__flow'),
+#                                plan = F('build_flow__build__plan__name'),
+#                                flow = F('build_flow__flow'),
 #                                branch = F('build_flow__build__branch__name'),
                                 )\
                                 .annotate(count = Count('id'), avg = Avg(metric))
