55cd05a26043e4b88585a03bf827c95af3e715e3
Paul Prescod <pprescod@salesforce.com>
2019-03-30 03:34:22 -0700

    Carve out text input into its own field.

---
+++ src/js/components/perfPages/formFields/textInput.js
@@ -0,0 +1,49 @@
+// @flow
+
+import * as React from 'react';
+
+import debounce from 'lodash/debounce';
+import { useState } from 'react';
+// flowlint  untyped-import:off
+import Input from '@salesforce/design-system-react/components/input';
+import Tooltip from '@salesforce/design-system-react/components/tooltip';
+// flowlint untyped-type-import:error
+
+type QueryBoundTextInputProps = {
+    label: string,
+    defaultValue: string,
+    onValueUpdate: (string) => void,
+    tooltip: string | null,
+}
+
+const QueryBoundTextInput = ({ label,
+            defaultValue,
+            onValueUpdate,
+            tooltip } : QueryBoundTextInputProps) => {
+    // debounce to reduce redraws while typing
+    let debouncedCallback = debounce((value: string) => onValueUpdate(value), 1000)
+
+    // store in state so debouncer can have internal history
+    let [debouncedChangeUrl, setDebouncer] = useState(
+        // wrap in obj to prevent magic useState behaviour
+        { debouncedCallback }
+    );
+    // unwrap
+    debouncedCallback = debouncedChangeUrl.debouncedCallback;
+
+    return <Input
+        label={label}
+        fieldLevelHelpTooltip={
+            tooltip &&
+            <Tooltip
+                align="top left"
+                content={tooltip}
+            />
+        }
+        defaultValue={defaultValue}
+        onChange={(event: null, { value }:{value:string}) =>
+                    debouncedCallback(value)}
+    />
+}
+
+export default QueryBoundTextInput;
