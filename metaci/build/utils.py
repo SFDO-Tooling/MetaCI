5f18c775028b553ef83bacd1ec95a9174937cfca
Paul Prescod <pprescod@salesforce.com>
2019-04-07 07:44:30 -0700

    Clear up deprecation warnings.

--- metaci/build/utils.py
+++ metaci/build/utils.py
@@ -11,8 +11,8 @@ from django.db.models import Q
 
 
 def paginate(build_list, request):
-    page = request.GET.get('page')
-    per_page = request.GET.get('per_page', '25')
+    page = request.GET.get("page")
+    per_page = request.GET.get("per_page", "25")
     paginator = Paginator(build_list, int(per_page))
     try:
         builds = paginator.page(page)
@@ -31,31 +31,26 @@ def set_build_info(build, **kwargs):
     build.save()
 
 
-def view_queryset(request, query=None, status=None, filter_class=None):
+def view_queryset(request, query=None, status=None, filterset_class=None):
     if not query:
         query = {}
 
-    Build = apps.get_model('build', 'Build')
-    builds = Build.objects.for_user(request.user, 'plan.view_builds')
+    Build = apps.get_model("build", "Build")
+    builds = Build.objects.for_user(request.user, "plan.view_builds")
     if query:
         builds = builds.filter(**query)
     if status:
         builds = builds.filter(
-            Q(
-                current_rebuild__isnull=True,
-                status=status,
-            ) | Q(
-                current_rebuild__isnull=False,
-                current_rebuild__status=status,
-            ),
+            Q(current_rebuild__isnull=True, status=status)
+            | Q(current_rebuild__isnull=False, current_rebuild__status=status)
         )
 
-    order_by = request.GET.get('order_by', '-time_queue')
-    order_by = order_by.split(',')
+    order_by = request.GET.get("order_by", "-time_queue")
+    order_by = order_by.split(",")
     builds = builds.order_by(*order_by)
 
-    if filter_class:
-        build_filter = filter_class(request.GET, builds)
+    if filterset_class:
+        build_filter = filterset_class(request.GET, builds)
         paginated = paginate(build_filter.qs, request)
         return build_filter, paginated
     else:
@@ -64,33 +59,31 @@ def view_queryset(request, query=None, status=None, filter_class=None):
 
 
 def format_log(log):
-    conv = Ansi2HTMLConverter(dark_bg=False, scheme='solarized', markup_lines=True)
+    conv = Ansi2HTMLConverter(dark_bg=False, scheme="solarized", markup_lines=True)
     headers = conv.produce_headers()
     content = conv.convert(log, full=False)
     content = '<pre class="ansi2html-content">{}</pre>'.format(content)
     return headers + content
 
+
 def run_command(command, env=None, cwd=None):
     kwargs = {}
     if env:
-        kwargs['env'] = env
+        kwargs["env"] = env
     if cwd:
-        kwargs['cwd'] = cwd
+        kwargs["cwd"] = cwd
     p = subprocess.Popen(
         command,
         stdout=subprocess.PIPE,
         bufsize=1,
         shell=True,
-        executable='/bin/bash',
+        executable="/bin/bash",
         **kwargs
     )
-    for line in iter(p.stdout.readline, ''):
+    for line in iter(p.stdout.readline, ""):
         yield line
     p.stdout.close()
     p.wait()
     if p.returncode:
-        message = 'Return code: {}\nstderr: {}'.format(
-            p.returncode,
-            p.stderr,
-        )
+        message = "Return code: {}\nstderr: {}".format(p.returncode, p.stderr)
         raise CommandException(message)
