e1ef839724786a5c958fd5612b702f25732d7812
Paul Prescod <pprescod@salesforce.com>
2019-04-12 13:41:50 -0700

    Move this file because it relates to APIs.

--- config/settings/common.py
+++ config/settings/common.py
@@ -348,7 +348,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 100,
     "DEFAULT_PERMISSION_CLASSES": (
-        "metaci.permissions.IsOnSecureNetwork",
+        "metaci.api.permissions.IsOnSecureNetwork",
         "rest_framework.permissions.IsAdminUser",
     ),
 }
