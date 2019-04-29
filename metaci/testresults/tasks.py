9fcd0315f7b2fc01d0013f657ba0962f235a9ada
Paul Prescod <pprescod@salesforce.com>
2019-04-29 07:54:10 -0700

    Ensure all of last week's records are summarized.

--- metaci/testresults/tasks.py
+++ metaci/testresults/tasks.py
@@ -1,9 +1,14 @@
 from django_rq import job
 from metaci.testresults.models import TestResultPerfWeeklySummary
+from datetime import datetime, timedelta
 
 
 @job
 def generate_summaries():
     """ An RQ repeatable task to summarize test results for faster querying.
     """
-    TestResultPerfWeeklySummary.summarize_weeks()
+    TestResultPerfWeeklySummary.summarize_week(datetime.now())
+    is_sunday = datetime.now().weekday() == 6
+    if is_sunday:
+        a_day_last_week = datetime.now() - timedelta(days=2)
+        TestResultPerfWeeklySummary.summarize_week(a_day_last_week)
