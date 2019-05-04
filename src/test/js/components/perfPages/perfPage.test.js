5f772468725a3c0f24883ea95a7996b334500c33
Paul Prescod <pprescod@salesforce.com>
2019-05-04 04:20:52 -0700

    Fix redux structures.

--- src/test/js/components/perfPages/perfPage.test.js
+++ src/test/js/components/perfPages/perfPage.test.js
@@ -3,12 +3,10 @@ import { MemoryRouter } from 'react-router-dom';
 import fetchMock from 'fetch-mock';
 
 import { renderWithRedux, storeWithApi } from '../../utils';
-
 import testmethod_perf_UI_data from '../../api/testmethod_perf_UI.json';
 import testmethod_perf_data from '../../api/testmethod_perf.json';
 
 import PerfPage from 'components/perfPages/perfPage';
-
 
 const initialStoreState = {
   user: {},
@@ -16,10 +14,15 @@ const initialStoreState = {
   perfDataUI: { status: 'LOADING', uidata: null },
 };
 
+const mockHTTP = () => {
+  fetchMock.getOnce('begin:/api/testmethod_perf', 200);
+  fetchMock.getOnce('begin:/api/testmethod_perf_UI', 200);
+};
+
 describe('Perf Page', () => {
   test('Initial Render', () => {
-    fetchMock.mock('*', 200);
-    const { getByText } = renderWithRedux(
+    mockHTTP();
+    const { getByText, queryByText } = renderWithRedux(
       <MemoryRouter>
         <PerfPage />
       </MemoryRouter>,
@@ -31,9 +34,13 @@ describe('Perf Page', () => {
     expect(getByText('Filters')).toBeVisible();
     expect(getByText('Date Range')).toBeVisible();
     expect(getByText('Options')).toBeVisible();
+    expect(queryByText('Success percentage above')).toBeNull();
   });
-  test('Render UI without data', () => {
-    const store = { ...initialStoreState, perfDataUI: testmethod_perf_UI_data };
+  test('Render UI from store without method data', () => {
+    const store = {
+      ...initialStoreState,
+      perfDataUI: { status: 'AVAILABLE', uidata: testmethod_perf_UI_data },
+    };
     fetchMock.mock('*', 200);
     const { getByText } = renderWithRedux(
       <MemoryRouter>
@@ -47,9 +54,13 @@ describe('Perf Page', () => {
     expect(getByText('Filters')).toBeVisible();
     expect(getByText('Date Range')).toBeVisible();
     expect(getByText('Options')).toBeVisible();
+    expect(getByText('Success percentage above')).toBeVisible();
   });
-  test('TODO: Render data without UI', () => {
-    const store = { ...initialStoreState, perfData: testmethod_perf_data };
+  test('Render data from store without UI', () => {
+    const store = {
+      ...initialStoreState,
+      perfData: { status: 'AVAILABLE', perfdata: testmethod_perf_data },
+    };
 
     fetchMock.mock('*', 200);
     const { getByText } = renderWithRedux(
@@ -64,14 +75,20 @@ describe('Perf Page', () => {
     expect(getByText('Filters')).toBeVisible();
     expect(getByText('Date Range')).toBeVisible();
     expect(getByText('Options')).toBeVisible();
+    expect(getByText('accFinderBuildsCorrectSearchQuery')).toBeVisible();
   });
-  test('TODO: Render data and UI', () => {
+  test('Render data and UI both from store', () => {
     fetchMock.mock('*', 200);
+    const store = {
+      ...initialStoreState,
+      perfData: { status: 'AVAILABLE', perfdata: testmethod_perf_data },
+      perfDataUI: { status: 'AVAILABLE', uidata: testmethod_perf_UI_data },
+    };
     const { getByText } = renderWithRedux(
       <MemoryRouter>
         <PerfPage />
       </MemoryRouter>,
-      initialStoreState,
+      store,
       storeWithApi,
     );
 
@@ -79,5 +96,9 @@ describe('Perf Page', () => {
     expect(getByText('Filters')).toBeVisible();
     expect(getByText('Date Range')).toBeVisible();
     expect(getByText('Options')).toBeVisible();
+    expect(getByText('accFinderBuildsCorrectSearchQuery')).toBeVisible();
+  });
+  test('Render data and UI from server', () => {
+
   });
 });
