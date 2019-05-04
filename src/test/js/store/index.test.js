6405862355311502982dca6ee1ee703d5caece29
Paul Prescod <pprescod@salesforce.com>
2019-05-03 19:18:33 -0700

    First round of tests.

---
+++ src/test/js/store/index.test.js
@@ -0,0 +1,9 @@
+import reducer from 'store';
+
+describe('reducer', () => {
+  test('combines child reducers', () => {
+    const actual = reducer(undefined, {});
+
+    expect(Object.keys(actual)).toEqual(['user', 'perfData', 'perfDataUI']);
+  });
+});
