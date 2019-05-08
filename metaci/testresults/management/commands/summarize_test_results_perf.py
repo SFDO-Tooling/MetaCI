b879f56b9c8e87626d479bc1e959bc46ac8c5f83
Paul Prescod <pprescod@salesforce.com>
2019-05-08 11:32:43 -0700

    🛢 First half of remove-buildflows.

---
+++ metaci/testresults/management/commands/summarize_test_results_perf.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+from metaci.testresults.models import TestResultPerfWeeklySummary
+
+
+class Command(BaseCommand):
+    # NOTE: This should only be run at heroku postdeploy stage
+    help = "Build rows in testresult_perfsummaries table for a single date"
+
+    def add_arguments(self, parser):
+        parser.add_argument("startdate", type=str)
+        parser.add_argument("enddate", type=str)
+        parser.add_argument("replace", choices=["replace", "continue"])
+
+    def handle(self, startdate, enddate, replace, **options):
+        TestResultPerfWeeklySummary.summarize_weeks(startdate, enddate)
