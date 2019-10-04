5b4ca0ebb356f06e0c10f54fdf4acd7cc2a8cb47
David Glick <dglick@salesforce.com>
2019-10-04 09:33:54 -0400

    Set build num

--- heroku_ci.sh
+++ heroku_ci.sh
@@ -5,14 +5,23 @@ git clone -b "$HEROKU_TEST_RUN_BRANCH" --single-branch https://github.com/SFDO-T
 cd MetaCI_checkout
 git reset --hard $HEROKU_TEST_RUN_COMMIT_VERSION
 export DJANGO_SETTINGS_MODULE=config.settings.test
+
+# Enable coveralls parallel mode so we can report for both Python & JS
 export COVERALLS_PARALLEL=true
+# Coveralls doesn't recognize the Heroku CI environment automatically.
+# So let's pretend we're CircleCI.
+export CIRCLECI=true
+export CIRCLE_BUILD_NUM=$HEROKU_TEST_RUN_ID
+
+# Run Python tests
 coverage run $(which pytest) --tap-stream
 exit_status=$?
 coveralls
 
+# Run JS tests
 yarn test:coverage
-yarn test:report-coverage
 exit_status=exit_status || $?
+yarn test:report-coverage
 
 curl -k "https://coveralls.io/webhook?repo_token=${COVERALLS_REPO_TOKEN}" -d "payload[build_num]=${HEROKU_TEST_RUN_ID}&payload[status]=done"
 if [ "$exit_status" != "0" ]; then
