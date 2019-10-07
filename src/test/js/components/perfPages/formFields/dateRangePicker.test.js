4c5390e1064b03ee786d53cf797171b0334f32bf
Paul Prescod <pprescod@salesforce.com>
2019-10-07 16:58:42 -0700

    Fix test.

--- src/test/js/components/perfPages/formFields/dateRangePicker.test.js
+++ src/test/js/components/perfPages/formFields/dateRangePicker.test.js
@@ -5,12 +5,12 @@ import DateRangePicker from '../../../../../js/components/perfPages/formFields/d
 
 describe('Choice Field', () => {
   test('Renders simple DateRangePicker', () => {
-    const { getByPlaceholderText } = render(<DateRangePicker />);
-    expect(getByPlaceholderText('Pick a Date')).toBeVisible();
+    const { getAllByPlaceholderText } = render(<DateRangePicker />);
+    expect(getAllByPlaceholderText('Pick a Date')).toHaveLength(2);
   });
 
   test('Renders DateRangePicker with defaults', () => {
-    const { getByPlaceholderText, getByValue } = render(
+    const { getAllByPlaceholderText, getByDisplayValue } = render(
       <div>
         1. <DateRangePicker startValue="2019-01-20T02:00" />
         2. <DateRangePicker endValue="2019-01-31T00:01" />
@@ -21,11 +21,11 @@ describe('Choice Field', () => {
         />
       </div>,
     );
-    expect(getByPlaceholderText('Pick a Date')).toBeVisible();
-    expect(getByValue('1/20/2019')).toBeVisible();
-    expect(getByValue('1/31/2019')).toBeVisible();
-    expect(getByValue('5/20/2019')).toBeVisible();
+    expect(getAllByPlaceholderText('Pick a Date')).toHaveLength(6);
+    expect(getByDisplayValue('1/20/2019')).toBeVisible();
+    expect(getByDisplayValue('1/31/2019')).toBeVisible();
+    expect(getByDisplayValue('5/20/2019')).toBeVisible();
     // the following shows some time zone issues to work through!
-    expect(getByValue('10/1/2019')).toBeVisible();
+    expect(getByDisplayValue('10/1/2019')).toBeVisible();
   });
 });
