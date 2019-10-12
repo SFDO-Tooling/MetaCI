e8b678893b09c2a45506f0a38c2357b94591d873
Paul Prescod <pprescod@salesforce.com>
2019-10-11 20:59:48 -0700

    Successful conversion.

---
+++ src/js/utils/logging.ts
@@ -0,0 +1,12 @@
+
+
+export const logError = (message: string | Error | Response, data: {
+  [Key: string]: unknown;
+} = {}) => {
+  // Raven/sentry is not configured for this project yet.
+  window.console.error(message, data);
+};
+
+export const log = (...args: Array<unknown>) => {
+  window.console.info(...args);
+};
