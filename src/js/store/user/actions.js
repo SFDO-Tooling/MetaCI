a9b55ff14619e48244da01b4c9931cd457409d17
Paul Prescod <pprescod@salesforce.com>
2019-10-11 19:17:03 -0700

    Remove Raven/Sentry

--- src/js/store/user/actions.js
+++ src/js/store/user/actions.js
@@ -7,33 +7,17 @@ type LoginAction = { type: 'USER_LOGGED_IN', payload: User };
 type LogoutAction = { type: 'USER_LOGGED_OUT' };
 export type UserAction = LoginAction | LogoutAction;
 
-export const login = (payload: User): LoginAction => {
-  if (window.Raven && window.Raven.isSetup()) {
-    window.Raven.setUserContext(payload);
-  }
-  /* istanbul ignore else */
-  if (payload && window.socket) {
-    window.socket.subscribe({
-      model: 'user',
-      id: payload.id,
-    });
-  }
-  return {
+export const login = (payload: User): LoginAction =>
+  // Raven/Sentry is not yet enabled for this project.
+  ({
     type: 'USER_LOGGED_IN',
     payload,
-  };
-};
+  });
 
 export const logout = (): ThunkAction => (dispatch, _getState, { apiFetch }) =>
   apiFetch(window.api_urls.account_logout(), {
     method: 'POST',
-  }).then(() => {
-    /* istanbul ignore else */
-    if (window.socket) {
-      window.socket.reconnect();
-    }
-    if (window.Raven && window.Raven.isSetup()) {
-      window.Raven.setUserContext();
-    }
-    return dispatch({ type: 'USER_LOGGED_OUT' });
-  });
+  }).then(() =>
+    // Raven/Sentry is not yet enabled for this project.
+    dispatch({ type: 'USER_LOGGED_OUT' }),
+  );
