036fc50dba189ec3f24c74cda5c13be4d056a2af
Paul Prescod <pprescod@salesforce.com>
2019-10-11 21:18:42 -0700

    DebugIcon to tsx.

---
+++ src/js/components/perfPages/debugIcon.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { connect } from "react-redux";
+import Icon from "@salesforce/design-system-react/components/icon";
+import { ComponentType } from "react";
+
+import { selectDebugStatus, selectPerfDataAPIUrl } from "store/perfdata/selectors";
+import { AppState } from "store";
+
+type ReduxProps = {
+  debugStatus: boolean;
+  apiURL: string;
+};
+
+const UnwrappedDebugIcon = ({
+  debugStatus,
+  apiURL
+}: ReduxProps) => debugStatus ? <div style={{ textAlign: 'right' }}>
+      <a href={apiURL} target="_debugCodeWindow">
+        <Icon assistiveText={{ label: 'debug' }} category="utility" name="cases" />
+      </a>
+    </div> : null;
+
+const select = (appState: AppState) => ({
+  debugStatus: selectDebugStatus(appState),
+  apiURL: selectPerfDataAPIUrl(appState)
+});
+
+const DebugIcon: ComponentType<{}> = connect(select, null)(UnwrappedDebugIcon);
+
+export default DebugIcon;
