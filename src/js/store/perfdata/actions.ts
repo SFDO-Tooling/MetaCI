83eed28dd7018357808251df0cdfdc811d147ddd
Paul Prescod <pprescod@salesforce.com>
2019-10-15 10:26:34 -0700

    Typescript types.

--- src/js/store/perfdata/actions.ts
+++ src/js/store/perfdata/actions.ts
@@ -1,12 +1,13 @@
 
 
 import queryString from "query-string";
-import { ThunkAction } from "redux-thunk";
 
 import { PerfData } from "store/perfdata/reducer";
 import { UIData } from "api/testmethod_perf_UI_JSON_schema";
 import { assertPerfData } from "api/testmethod_perfdata_JSON_schema";
 import { assertUIData } from "api/testmethod_perf_UI_JSON_schema";
+import { ThunkDispatch } from 'redux-thunk'
+import { AnyAction } from 'redux';
 
 export const testmethod_perfdata_url = '/api/testmethod_perf';
 export const testmethod_perf_UI_url = '/api/testmethod_perf_UI';
@@ -31,9 +32,11 @@ type UIDataError = {type: "UI_DATA_ERROR";payload: string;};
 export type PerfDataAction = PerfDataAvailableAction | PerfDataLoadingAction | PerfDataError;
 export type UIDataAction = UIDataAvailableAction | UIDataLoadingAction | UIDataError;
 
+type ReduxDispatch = ThunkDispatch<{}, {}, AnyAction>;
+
 /* eslint-disable no-use-before-define */
 
-export const perfRESTFetch = (url: string, params?: {}) => (dispatch, _getState, {
+export const perfRESTFetch = (url: string, params?: {}) => (dispatch : ReduxDispatch, _getState : any, {
   apiFetch
 }) => {
   if (params) {
@@ -48,9 +51,9 @@ export const perfRESTFetch = (url: string, params?: {}) => (dispatch, _getState,
   });
 };
 
-export const perfREST_UI_Fetch = (params?: {}) => (dispatch, _getState, {
+export const perfREST_UI_Fetch = (params?: {}) => (dispatch : ReduxDispatch, _getState: any, {
   apiFetch
-}) => {
+} : {apiFetch : any}) => {
   let url = testmethod_perf_UI_url;
   if (params) {
     url = `${url}?${queryString.stringify(params)}`;
@@ -74,14 +77,14 @@ export const perfREST_API = ({
   url,
   checkValid
 }: {
-  dispatch: UntypedFunc;
+  dispatch: ReduxDispatch;
   apiFetch: UntypedFunc;
   prefix: string;
   url: string;
   checkValid: UntypedFunc;
 }) => {
   dispatch({ type: `${prefix}_DATA_LOADING`, payload: { url } });
-  return apiFetch(url, { method: 'GET' }).then(payload => {
+  return apiFetch(url, { method: 'GET' }).then((payload:any) => {
     try {
       if (!payload) {
         throw new Error('No payload');
