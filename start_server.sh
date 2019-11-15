9731bc3ded28a7104e9593ecf4f4cefc6dd1d6d5
jofsky <josh.kofsky@gmail.com>
2019-11-14 16:53:22 -0800

    pushing docker-compose file updates

--- start_server.sh
+++ start_server.sh
@@ -6,6 +6,7 @@ if [ "${BUILD_ENV}" = "test" ] ; then
     python /app/manage.py populate_db
     python /app/manage.py metaci_scheduled_jobs
     # python /app/manage.py createsuperuser
+    # echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@salesforce.com', 'password')" | python manage.py shell
 fi
 yarn serve
 
