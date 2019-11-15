171240de5102f216651a6fbd90097b9b10be9f3c
jofsky <josh.kofsky@gmail.com>
2019-11-14 19:07:44 -0800

    adding test development admin profile by default

--- start_server.sh
+++ start_server.sh
@@ -2,11 +2,11 @@
 export PROJECT=$(pwd)
 export PATH=$PROJECT/node/bin:$PROJECT/node_modules/.bin:$PATH
 python /app/manage.py migrate
-if [ "${BUILD_ENV}" = "test" ] ; then
-    python /app/manage.py populate_db
-    python /app/manage.py metaci_scheduled_jobs
-    # python /app/manage.py createsuperuser
-    # echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@salesforce.com', 'password')" | python manage.py shell
+
+if [ "${BUILD_ENV}" = "development" ] ; then
+    python /app/manage.py populate_db;
+    python /app/manage.py metaci_scheduled_jobs;
+    echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@salesforce.com', 'password')" | python manage.py shell
 fi
 yarn serve
 
