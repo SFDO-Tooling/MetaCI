c644366f489cc3c603b96c60e52b866c4086d8d3
jofsky <josh.kofsky@gmail.com>
2019-11-26 14:43:56 -0800

    recompiling node-sass

--- utility/start_server.sh
+++ utility/start_server.sh
@@ -22,6 +22,7 @@ echo $SFDX_HUB_KEY > /app/sfdx_hub.key
 echo "AUTHORIZING SFDX via JWT key auth..."
 # authorizing sfdx via jwt key authorization
 sfdx force:auth:jwt:grant -u $SFDX_HUB_USERNAME -f /app/sfdx_hub.key -i $SFDX_CLIENT_ID --setdefaultdevhubusername
+npm rebuild node-sass
 # starting server and webpack
 yarn serve
 echo "ALL DONE!"
