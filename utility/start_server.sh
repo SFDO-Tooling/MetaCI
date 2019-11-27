8e428da9cd6fb3df7a7a47776ee2498eac781eef
jofsky <josh.kofsky@gmail.com>
2019-11-26 21:53:23 -0800

    pushing polished gitignore and start_server.sh

--- utility/start_server.sh
+++ utility/start_server.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# running django database migrations 
 python /app/manage.py migrate
-echo "BUILD_ENV: ${BUILD_ENV}"
-if [ "${BUILD_ENV}" = "development" ] ; then
+# running job scheduler 
+python /app/manage.py metaci_scheduled_jobs;
+# creating test data if local settings are configured
+if [ "${DJANGO_SETTINGS_MODULE}" = "config.settings.local" ] ; then
     echo "CREATING ADMIN USER FOR TESTING PURPOSES..."
     # Using key error as an indicator for whether or not to run database population and job scheduling scripts
     echo "from django.contrib.auth import get_user_model; User = get_user_model();User.objects.create_superuser('admin', 'admin@salesforce.com', 'password')" | python manage.py shell
@@ -10,8 +13,6 @@ if [ "${BUILD_ENV}" = "development" ] ; then
         # populating database with test repository, done only once
         echo "POPULATING DATABASE WITH TEST DATA..."
         python /app/manage.py populate_db;
-        # running job scheduler 
-        python /app/manage.py metaci_scheduled_jobs;
         npm rebuild node-sass
     else
         # Redirect stdout from echo command to stderr.
