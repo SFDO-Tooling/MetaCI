9725f1acd909d6bc9ecb15d67d0171c4ffe19541
Bryan Oakley <bryan.oakley@gmail.com>
2020-07-06 17:29:24 -0500

    Improved robot api tests - they weren't setting message and keyword. D'oh! Also added a test that has some unicode characters in it.

--- metaci/api/tests/test_api_robot.py
+++ metaci/api/tests/test_api_robot.py
@@ -1,13 +1,15 @@
 """Test cases for /api/robot, mostly focusing on csv output"""
 
 from rest_framework.test import APIClient, APITestCase
+
 from metaci.conftest import (
     BranchFactory,
     RepositoryFactory,
+    StaffSuperuserFactory,
+    TestResultFactory,
     UserFactory,
 )
 from metaci.testresults.models import TestResult
-from metaci.conftest import StaffSuperuserFactory, TestResultFactory
 
 
 class TestAPIRobot(APITestCase):
@@ -33,6 +35,7 @@ class TestAPIRobot(APITestCase):
             (repo2, "file2.robot", "Fail", feature, "Failing 1", "KW1", "epic fail",),
             (repo2, "file2.robot", "Fail", feature, "Failing 2", "KW1", "epic fail",),
             (repo2, "file3.robot", "Fail", feature, "Failing 3", "KW2", "epic fail",),
+            (repo2, "file3.robot", "Fail", feature, "Failing 4", "KW3", "ʃıɐɟ ɔıdǝ",),
         ):
             TestResultFactory(
                 method__testclass__test_type="Robot",
@@ -41,7 +44,8 @@ class TestAPIRobot(APITestCase):
                 method__name=test_name,
                 outcome=outcome,
                 source_file=source,
-                robot_keyword=None,
+                robot_keyword=robot_keyword,
+                message=message,
             )
 
     def test_superuser_access(self):
@@ -74,76 +78,80 @@ class TestAPIRobot(APITestCase):
         assert (
             TestResult.objects.filter(method__testclass__test_type="Apex").count() > 0
         )
-        assert data["count"] == 5
+        assert data["count"] == 6
 
     def test_result_csv_format(self):
         """Verify we can get back csv results"""
         self.client.force_authenticate(self.superuser)
         response = self.client.get("/api/robot.csv")
         expected = [
-            b"id,repo_name,branch_name,outcome,source_file,test_name,message,robot_keyword",
-            b"2,repo1,master,Pass,file1.robot,Passing 1,,",
-            b"3,repo1,master,Pass,file1.robot,Passing 2,,",
-            b"4,repo2,feature/robot,Fail,file2.robot,Failing 1,,",
-            b"5,repo2,feature/robot,Fail,file2.robot,Failing 2,,",
-            b"6,repo2,feature/robot,Fail,file3.robot,Failing 3,,",
+            "id,repo_name,branch_name,outcome,source_file,test_name,message,robot_keyword",
+            "2,repo1,master,Pass,file1.robot,Passing 1,,",
+            "3,repo1,master,Pass,file1.robot,Passing 2,,",
+            "4,repo2,feature/robot,Fail,file2.robot,Failing 1,epic fail,KW1",
+            "5,repo2,feature/robot,Fail,file2.robot,Failing 2,epic fail,KW1",
+            "6,repo2,feature/robot,Fail,file3.robot,Failing 3,epic fail,KW2",
+            "7,repo2,feature/robot,Fail,file3.robot,Failing 4,ʃıɐɟ ɔıdǝ,KW3",
         ]
-        actual = response.content.splitlines()
+        actual = response.content.decode().splitlines()
         self.assertListEqual(expected, actual)
 
     def test_repo_filter(self):
         self.client.force_authenticate(self.superuser)
         response = self.client.get("/api/robot.csv?repo_name=repo2")
         expected = [
-            b"id,repo_name,branch_name,outcome,source_file,test_name,message,robot_keyword",
-            b"4,repo2,feature/robot,Fail,file2.robot,Failing 1,,",
-            b"5,repo2,feature/robot,Fail,file2.robot,Failing 2,,",
-            b"6,repo2,feature/robot,Fail,file3.robot,Failing 3,,",
+            "id,repo_name,branch_name,outcome,source_file,test_name,message,robot_keyword",
+            "4,repo2,feature/robot,Fail,file2.robot,Failing 1,epic fail,KW1",
+            "5,repo2,feature/robot,Fail,file2.robot,Failing 2,epic fail,KW1",
+            "6,repo2,feature/robot,Fail,file3.robot,Failing 3,epic fail,KW2",
+            "7,repo2,feature/robot,Fail,file3.robot,Failing 4,ʃıɐɟ ɔıdǝ,KW3",
         ]
 
-        actual = response.content.splitlines()
+        actual = response.content.decode().splitlines()
         self.assertListEqual(expected, actual)
 
     def test_branch_filter(self):
         self.client.force_authenticate(self.superuser)
         response = self.client.get("/api/robot.csv?branch_name=master")
         expected = [
-            b"id,repo_name,branch_name,outcome,source_file,test_name,message,robot_keyword",
-            b"2,repo1,master,Pass,file1.robot,Passing 1,,",
-            b"3,repo1,master,Pass,file1.robot,Passing 2,,",
+            "id,repo_name,branch_name,outcome,source_file,test_name,message,robot_keyword",
+            "2,repo1,master,Pass,file1.robot,Passing 1,,",
+            "3,repo1,master,Pass,file1.robot,Passing 2,,",
         ]
 
-        actual = response.content.splitlines()
+        actual = response.content.decode().splitlines()
         self.assertListEqual(expected, actual)
 
     def test_outcome_filter(self):
         self.client.force_authenticate(self.superuser)
         response = self.client.get("/api/robot.csv?outcome=Fail")
         expected = [
-            b"id,repo_name,branch_name,outcome,source_file,test_name,message,robot_keyword",
-            b"4,repo2,feature/robot,Fail,file2.robot,Failing 1,,",
-            b"5,repo2,feature/robot,Fail,file2.robot,Failing 2,,",
-            b"6,repo2,feature/robot,Fail,file3.robot,Failing 3,,",
+            "id,repo_name,branch_name,outcome,source_file,test_name,message,robot_keyword",
+            "4,repo2,feature/robot,Fail,file2.robot,Failing 1,epic fail,KW1",
+            "5,repo2,feature/robot,Fail,file2.robot,Failing 2,epic fail,KW1",
+            "6,repo2,feature/robot,Fail,file3.robot,Failing 3,epic fail,KW2",
+            "7,repo2,feature/robot,Fail,file3.robot,Failing 4,ʃıɐɟ ɔıdǝ,KW3",
         ]
-        actual = response.content.splitlines()
+        actual = response.content.decode().splitlines()
         self.assertListEqual(expected, actual)
 
     def test_test_name_filter(self):
         self.client.force_authenticate(self.superuser)
         response = self.client.get("/api/robot.csv?test_name=Failing 2")
         expected = [
-            b"id,repo_name,branch_name,outcome,source_file,test_name,message,robot_keyword",
-            b"5,repo2,feature/robot,Fail,file2.robot,Failing 2,,",
+            "id,repo_name,branch_name,outcome,source_file,test_name,message,robot_keyword",
+            "5,repo2,feature/robot,Fail,file2.robot,Failing 2,epic fail,KW1",
         ]
-        actual = response.content.splitlines()
+        actual = response.content.decode().splitlines()
         self.assertListEqual(expected, actual)
 
     def test_source_file_filter(self):
         self.client.force_authenticate(self.superuser)
         response = self.client.get("/api/robot.csv?source_file=file3.robot")
         expected = [
-            b"id,repo_name,branch_name,outcome,source_file,test_name,message,robot_keyword",
-            b"6,repo2,feature/robot,Fail,file3.robot,Failing 3,,",
+            "id,repo_name,branch_name,outcome,source_file,test_name,message,robot_keyword",
+            "6,repo2,feature/robot,Fail,file3.robot,Failing 3,epic fail,KW2",
+            "7,repo2,feature/robot,Fail,file3.robot,Failing 4,ʃıɐɟ ɔıdǝ,KW3",
         ]
-        actual = response.content.splitlines()
+        actual = response.content.decode().splitlines()
         self.assertListEqual(expected, actual)
