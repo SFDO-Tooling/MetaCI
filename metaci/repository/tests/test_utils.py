8922f54d39f6afee10600f1372ddb9542c77d7e7
Brandon Parker <brandon.parker@salesforce.com>
2020-11-25 15:15:18 -0800

    review and fix tests

--- metaci/repository/tests/test_utils.py
+++ metaci/repository/tests/test_utils.py
@@ -3,8 +3,14 @@ from unittest import mock
 import pytest
 
 from metaci.build.exceptions import BuildError
-from metaci.conftest import BuildFactory
+from metaci.conftest import BuildFactory, BuildFlowFactory
 from metaci.repository import utils
+
+
+@pytest.fixture(scope="module", autouse=True)
+def site_url():
+    with mock.patch("metaci.build.models.settings.SITE_URL", "initech.co"):
+        yield
 
 
 def setup_build_with_status(status):
@@ -24,164 +30,149 @@ def setup_build_with_status(status):
 def test_create_status__queued():
     build, repo = setup_build_with_status("queued")
 
-    with mock.patch("metaci.build.models.settings.SITE_URL", "initech.co"):
-        utils.create_status(build)
-        repo.create_status.assert_called_once_with(
-            sha=build.commit,
-            state="pending",
-            target_url=f"initech.co/builds/{build.id}",
-            description="The build is queued",
-            context=build.plan.context,
-        )
+    utils.create_status(build)
+
+    repo.create_status.assert_called_once_with(
+        sha=build.commit,
+        state="pending",
+        target_url=f"initech.co/builds/{build.id}",
+        description="The build is queued",
+        context=build.plan.context,
+    )
 
 
 @pytest.mark.django_db
 def test_create_status__waiting():
     build, repo = setup_build_with_status("waiting")
 
-    with mock.patch("metaci.build.models.settings.SITE_URL", "initech.co"):
-        utils.create_status(build)
-        repo.create_status.assert_called_once_with(
-            sha=build.commit,
-            state="pending",
-            target_url=f"initech.co/builds/{build.id}",
-            description="The build is waiting for another build to complete",
-            context=build.plan.context,
-        )
+    utils.create_status(build)
+    repo.create_status.assert_called_once_with(
+        sha=build.commit,
+        state="pending",
+        target_url=f"initech.co/builds/{build.id}",
+        description="The build is waiting for another build to complete",
+        context=build.plan.context,
+    )
 
 
 @pytest.mark.django_db
 def test_create_status__running():
     build, repo = setup_build_with_status("running")
 
-    with mock.patch("metaci.build.models.settings.SITE_URL", "initech.co"):
-        utils.create_status(build)
-        repo.create_status.assert_called_once_with(
-            sha=build.commit,
-            state="pending",
-            target_url=f"initech.co/builds/{build.id}",
-            description="The build is running",
-            context=build.plan.context,
-        )
+    utils.create_status(build)
+    repo.create_status.assert_called_once_with(
+        sha=build.commit,
+        state="pending",
+        target_url=f"initech.co/builds/{build.id}",
+        description="The build is running",
+        context=build.plan.context,
+    )
 
 
 @pytest.mark.django_db
 def test_create_status__qa():
     build, repo = setup_build_with_status("qa")
 
-    with mock.patch("metaci.build.models.settings.SITE_URL", "initech.co"):
-        utils.create_status(build)
-        repo.create_status.assert_called_once_with(
-            sha=build.commit,
-            state="pending",
-            target_url=f"initech.co/builds/{build.id}",
-            description=f"{build.user} is testing",
-            context=build.plan.context,
-        )
+    utils.create_status(build)
+    repo.create_status.assert_called_once_with(
+        sha=build.commit,
+        state="pending",
+        target_url=f"initech.co/builds/{build.id}",
+        description=f"{build.user} is testing",
+        context=build.plan.context,
+    )
 
 
 @pytest.mark.django_db
 def test_create_status__success():
     build, repo = setup_build_with_status("success")
 
-    with mock.patch("metaci.build.models.settings.SITE_URL", "initech.co"):
-        utils.create_status(build)
-        repo.create_status.assert_called_once_with(
-            sha=build.commit,
-            state="success",
-            target_url=f"initech.co/builds/{build.id}",
-            description="The build was successful",
-            context=build.plan.context,
-        )
+    utils.create_status(build)
+    repo.create_status.assert_called_once_with(
+        sha=build.commit,
+        state="success",
+        target_url=f"initech.co/builds/{build.id}",
+        description="The build was successful",
+        context=build.plan.context,
+    )
 
 
 @pytest.mark.django_db
 def test_create_status__success_qa():
     build, repo = setup_build_with_status("success")
     build.plan.role = "qa"
-    with mock.patch("metaci.build.models.settings.SITE_URL", "initech.co"):
-        utils.create_status(build)
-        repo.create_status.assert_called_once_with(
-            sha=build.commit,
-            state="success",
-            target_url=f"initech.co/builds/{build.id}",
-            description=f"{build.qa_user} approved. See details for QA comments",
-            context=build.plan.context,
-        )
+
+    utils.create_status(build)
+    repo.create_status.assert_called_once_with(
+        sha=build.commit,
+        state="success",
+        target_url=f"initech.co/builds/{build.id}",
+        description=f"{build.qa_user} approved. See details for QA comments",
+        context=build.plan.context,
+    )
 
 
 @pytest.mark.django_db
 def test_create_status__success_with_commit_status():
     build, repo = setup_build_with_status("success")
     build.commit_status = "This build is malarky"
-    with mock.patch("metaci.build.models.settings.SITE_URL", "initech.co"):
-        utils.create_status(build)
-        repo.create_status.assert_called_once_with(
-            sha=build.commit,
-            state="success",
-            target_url=f"initech.co/builds/{build.id}",
-            description="This build is malarky",
-            context=build.plan.context,
-        )
+
+    utils.create_status(build)
+    repo.create_status.assert_called_once_with(
+        sha=build.commit,
+        state="success",
+        target_url=f"initech.co/builds/{build.id}",
+        description="This build is malarky",
+        context=build.plan.context,
+    )
 
 
 @pytest.mark.django_db
 def test_create_status__error():
     build, repo = setup_build_with_status("error")
-    with mock.patch("metaci.build.models.settings.SITE_URL", "initech.co"):
-        utils.create_status(build)
-        repo.create_status.assert_called_once_with(
-            sha=build.commit,
-            state="error",
-            target_url=f"initech.co/builds/{build.id}",
-            description="💥  An error occurred during the build",
-            context=build.plan.context,
-        )
+
+    utils.create_status(build)
+    repo.create_status.assert_called_once_with(
+        sha=build.commit,
+        state="error",
+        target_url=f"initech.co/builds/{build.id}",
+        description="💥  An error occurred during the build",
+        context=build.plan.context,
+    )
 
 
 @pytest.mark.django_db
 def test_create_status__fail():
     build, repo = setup_build_with_status("fail")
-    build.tests_fail = 2
-    with mock.patch("metaci.build.models.settings.SITE_URL", "initech.co"):
-        utils.create_status(build)
-        repo.create_status.assert_called_once_with(
-            sha=build.commit,
-            state="failure",
-            target_url=f"initech.co/builds/{build.id}/tests",
-            description="⚠ ️There were 2 tests that failed.",
-            context=build.plan.context,
-        )
+    # First BuildFlow
+    BuildFlowFactory(build=build)
+    # Second BuildFlow, this one has a failing test :(
+    bf = BuildFlowFactory(build=build)
+    bf.tests_fail = 1
+    bf.save()
 
-
-@pytest.mark.django_db
-def test_create_status__fail_singular():
-    build, repo = setup_build_with_status("fail")
-    build.tests_fail = 1
-    with mock.patch("metaci.build.models.settings.SITE_URL", "initech.co"):
-        utils.create_status(build)
-        repo.create_status.assert_called_once_with(
-            sha=build.commit,
-            state="failure",
-            target_url=f"initech.co/builds/{build.id}/tests",
-            description="⚠ ️There was 1 test that failed.",
-            context=build.plan.context,
-        )
+    utils.create_status(build)
+    repo.create_status.assert_called_once_with(
+        sha=build.commit,
+        state="failure",
+        target_url=f"initech.co/builds/{build.id}/tests",
+        description="⚠ ️1/2 failed",  # each BuildFlow has total_tests == 1
+        context=build.plan.context,
+    )
 
 
 @pytest.mark.django_db
 def test_create_status__fail_qa():
     build, repo = setup_build_with_status("fail")
     build.plan.role = "qa"
-    with mock.patch("metaci.build.models.settings.SITE_URL", "initech.co"):
-        utils.create_status(build)
-        repo.create_status.assert_called_once_with(
-            sha=build.commit,
-            state="failure",
-            target_url=f"initech.co/builds/{build.id}",
-            description=f"{build.qa_user} rejected. See details for QA comments",
-            context=build.plan.context,
-        )
+    utils.create_status(build)
+    repo.create_status.assert_called_once_with(
+        sha=build.commit,
+        state="failure",
+        target_url=f"initech.co/builds/{build.id}",
+        description=f"{build.qa_user} rejected. See details for QA comments",
+        context=build.plan.context,
+    )
 
 
 @pytest.mark.django_db
--- metaci/repository/utils.py
+++ metaci/repository/utils.py
@@ -7,27 +7,29 @@ def create_status(build):
         return
 
     state = None
-    target_url = None
+    target_url = build.get_external_url()
     description = None
     repo = build.repo.get_github_api()
 
-    if build.get_status() == "queued":
+    build_status = build.get_status()
+
+    if build_status == "queued":
         state = "pending"
         description = "The build is queued"
 
-    elif build.get_status() == "waiting":
+    elif build_status == "waiting":
         state = "pending"
         description = "The build is waiting for another build to complete"
 
-    elif build.get_status() == "running":
+    elif build_status == "running":
         state = "pending"
         description = "The build is running"
 
-    elif build.get_status() == "qa":
+    elif build_status == "qa":
         state = "pending"
         description = f"{build.user} is testing"
 
-    elif build.get_status() == "success":
+    elif build_status == "success":
         state = "success"
         if build.commit_status:
             description = build.commit_status
@@ -36,28 +38,33 @@ def create_status(build):
         else:
             description = "The build was successful"
 
-    elif build.get_status() == "error":
+    elif build_status == "error":
         state = "error"
         description = "💥  An error occurred during the build"
 
-    elif build.get_status() == "fail":
+    elif build_status == "fail":
         state = "failure"
         if build.plan.role == "qa":
             description = f"{build.qa_user} rejected. See details for QA comments"
         else:
-            plural = build.tests_fail != 1
-            description = f"⚠ ️There {'were' if plural else 'was'} {build.tests_fail} test{'s' if plural else ''} that failed."
+            total_tests = 0
+            failed_tests = 0
+            for bf in build.flows.all():
+                if bf.tests_fail:
+                    failed_tests += bf.tests_fail
+                if bf.tests_total:
+                    total_tests += bf.tests_total
+
+            description = f"⚠ ️{failed_tests}/{total_tests} failed"
             target_url = f"{build.get_external_url()}/tests"
 
     else:
-        raise BuildError(
-            f"ERROR: Unrecognized build status encountered: {build.get_status()}"
-        )
+        raise BuildError(f"Unrecognized build status encountered: {build_status}")
 
     response = repo.create_status(
         sha=build.commit,
         state=state,
-        target_url=target_url or build.get_external_url(),
+        target_url=target_url,
         description=description,
         context=build.plan.context,
     )
