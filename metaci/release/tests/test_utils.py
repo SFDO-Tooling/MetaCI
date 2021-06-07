62b7536d27a5e77dc2ee94b945a335429ce7ff29
jofsky <josh.kofsky@gmail.com>
2021-06-07 14:36:34 -0700

    [MetaCI] Pushing so I don't have to set up locally on other laptop

--- metaci/release/tests/test_utils.py
+++ metaci/release/tests/test_utils.py
@@ -3,7 +3,7 @@ from unittest.mock import Mock
 import pytest
 
 from ...fixtures.factories import ReleaseFactory
-from ..utils import send_release_webhook
+from ..utils import send_release_webhook, send_submit_webhook
 
 
 def test_send_release_webhook(mocked_responses, mocker, transactional_db):
@@ -58,3 +58,59 @@ def test_send_release_webhook__error(mocked_responses, mocker, transactional_db)
         Exception, match="ImplementationStep matching query does not exist."
     ):
         send_release_webhook(release, "INFRA.instance1")
+
+
+def test_send_submit_webhook(mocked_responses, mocker, transactional_db):
+    mocker.patch(
+        "metaci.release.utils.settings",
+        METACI_RELEASE_WEBHOOK_URL="https://webhook",
+        METACI_CHANGE_CASE_URL_TEMPLATE="{case_id}",
+        METACI_RELEASE_WEBHOOK_ISSUER="MetaCI",
+        METACI_RELEASE_WEBHOOK_AUTH_KEY="test",
+    )
+    mocked_responses.add(
+        "POST",
+        "https://webhook/case/{release.change_case_link}/submit",
+        json={"success": True, "id": "2"},
+    )
+
+    project_config = Mock(project__package__name="Test Package")
+    project_config.get_version_for_tag.return_value = "1.0"
+    release = ReleaseFactory()
+
+    send_submit_webhook(release, "INFRA.instance1")
+
+    assert release.change_case_link == "2"
+
+
+def test_send_submit_webhook__disabled(mocked_responses):
+    send_submit_webhook(None, None)
+    assert len(mocked_responses.calls) == 0
+
+
+def test_send_submit_webhook__error(mocked_responses, mocker, transactional_db):
+    mocker.patch(
+        "metaci.release.utils.settings",
+        METACI_RELEASE_WEBHOOK_URL="https://webhook",
+        METACI_RELEASE_WEBHOOK_ISSUER="MetaCI",
+        METACI_RELEASE_WEBHOOK_AUTH_KEY="test",
+    )
+    mocked_responses.add(
+        "POST",
+        "https://webhook/case/0/submit",
+        json={
+            "success": False,
+            "errors": [
+                {"message": "ImplementationStep matching query does not exist."}
+            ],
+        },
+    )
+
+    project_config = Mock(project__package__name="Test Package")
+    project_config.get_version_for_tag.return_value = "1.0"
+    release = ReleaseFactory()
+
+    with pytest.raises(
+        Exception, match="ImplementationStep matching query does not exist."
+    ):
+        send_submit_webhook(release, "INFRA.instance1")
