3acdc676ea19c40098d09911013ef17fe4fb4a5a
Josh Kofsky <jkofsky@salesforce.com>
2021-06-07 13:55:30 -0700

    Update metaci/release/context_processors.py
    Co-authored-by: David Glick <dglick@salesforce.com>

--- metaci/release/context_processors.py
+++ metaci/release/context_processors.py
@@ -3,7 +3,6 @@ import os
 
 def get_release_values(request):
     return {
-        "METACI_START_STOP_WEBHOOK": os.environ.get("METACI_START_STOP_WEBHOOK", False),
         "METACI_RELEASE_WEBHOOK_URL": os.environ.get(
             "METACI_RELEASE_WEBHOOK_URL", None
         ),
