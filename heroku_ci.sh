2ec17826fdf899a46befade2894dc2032dfd4110
Brandon Parker <brandon.parker@salesforce.com>
2021-06-09 15:23:11 -0700

    remove extra newline

--- heroku_ci.sh
+++ heroku_ci.sh
@@ -43,9 +43,7 @@ then
 fi
 
 cat ./coverage/lcov.info | /app/node_modules/.bin/coveralls
-
 curl -k "https://coveralls.io/webhook?repo_token=${COVERALLS_REPO_TOKEN}" -d "payload[build_num]=${HEROKU_TEST_RUN_ID}&payload[status]=done"
-
 if [ "$exit_status" != "0" ]; then
     exit $exit_status
 fi
