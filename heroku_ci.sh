baa50acb230ff42d641da38ab49bd355b93ea28d
Brandon Parker <brandon.parker@salesforce.com>
2021-06-09 13:54:31 -0700

    properly assign return_code

--- heroku_ci.sh
+++ heroku_ci.sh
@@ -35,7 +35,11 @@ coveralls
 
 # Run JS tests
 yarn test:coverage
-exit_status=$exit_status || $?
+
+if [ "$exit_status" -eq 0 ]
+then
+    exit_status=$?
+fi
 cat ./coverage/lcov.info | /app/node_modules/.bin/coveralls
 
 curl -k "https://coveralls.io/webhook?repo_token=${COVERALLS_REPO_TOKEN}" -d "payload[build_num]=${HEROKU_TEST_RUN_ID}&payload[status]=done"
