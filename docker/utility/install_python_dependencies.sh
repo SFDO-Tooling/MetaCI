56c5f246acf1db3ac3d5cdec32063c09668e44da
RupertBarrow <rupert.barrow@lilo.org>
2021-06-23 21:54:30 +0200

    Installation env variables

--- docker/utility/install_python_dependencies.sh
+++ docker/utility/install_python_dependencies.sh
@@ -37,6 +37,7 @@ if [ -z "$VIRTUAL_ENV" ]; then
     exit 1;
 else
 
+    pip install django-debug-toolbar
     pip install -r $PROJECT_DIR/requirements/local.txt
     pip install -r $PROJECT_DIR/requirements/test.txt
     pip install -r $PROJECT_DIR/requirements.txt
