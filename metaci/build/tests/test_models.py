814d0ff78aba2fcad1293148be541aa97960b429
jofsky <josh.kofsky@gmail.com>
2021-08-10 15:57:19 -0400

    [MetaCI] Adding sexplicit teimes for push time options

--- metaci/build/tests/test_models.py
+++ metaci/build/tests/test_models.py
@@ -5,6 +5,7 @@ from unittest import mock
 
 import pytest
 from cumulusci.core.config import OrgConfig
+from django.utils import timezone
 
 from metaci.build.models import Build
 from metaci.conftest import (
@@ -17,7 +18,8 @@ from metaci.conftest import (
     RepositoryFactory,
     ScratchOrgInstanceFactory,
 )
-from metaci.release.models import Release, ChangeCaseTemplate, DefaultImplementationStep
+from metaci.release.models import ChangeCaseTemplate, Release
+
 
 @pytest.mark.django_db
 class TestBuild:
@@ -164,24 +166,27 @@ class TestBuildFlow:
             "production_date"
         ] == datetime.date.today() + datetime.timedelta(days=6)
 
-
-    def test_get_flow_options_push_sandbox(self):
+    def test_get_flow_options_push_sandbox_no_push_time(self):
         build_flow = BuildFlowFactory()
-        build_flow.build.plan = PlanFactory(role="push_sandbox", change_traffic_control=True)
+        build_flow.build.plan = PlanFactory(
+            role="push_sandbox", change_traffic_control=True
+        )
         build_flow.build.plan.save()
         build_flow.build.repo = RepositoryFactory(
             default_implementation_steps=[
                 {
                     "role": "push_sandbox",
                     "duration": 10,
-                    "push_time": 21,
+                    "push_time": None,
                     "start_time": 8,
                     "start_date_offset": 0,
                 },
             ],
         )
         build_flow.build.repo.save()
-        planrepo = PlanRepositoryFactory(plan=build_flow.build.plan, repo=build_flow.build.repo)
+        planrepo = PlanRepositoryFactory(
+            plan=build_flow.build.plan, repo=build_flow.build.repo
+        )
         planrepo.save()
         change_case_template = ChangeCaseTemplate()
         change_case_template.save()
@@ -191,12 +196,48 @@ class TestBuildFlow:
         )
         build_flow.build.release.version_number = "1.0"
         build_flow.build.release.save()
-        step = DefaultImplementationStep(**build_flow.build.repo.default_implementation_steps[0])
-        start = step.start(build_flow.build.release)
-        push_time = step._push_time(start)
         options = build_flow._get_flow_options()
         assert options["push_sandbox"]["version"] == "1.0"
-        assert options["push_sandbox"]["start_time"] == push_time
+        assert options["push_sandbox"]["start_time"] is None
+
+    def test_get_flow_options_push_sandbox(self):
+        build_flow = BuildFlowFactory()
+        build_flow.build.plan = PlanFactory(
+            role="push_sandbox", change_traffic_control=True
+        )
+        build_flow.build.plan.save()
+        build_flow.build.repo = RepositoryFactory(
+            default_implementation_steps=[
+                {
+                    "role": "push_sandbox",
+                    "duration": 10,
+                    "push_time": 20,
+                    "start_time": 8,
+                    "start_date_offset": 0,
+                },
+            ],
+        )
+        build_flow.build.repo.save()
+        planrepo = PlanRepositoryFactory(
+            plan=build_flow.build.plan, repo=build_flow.build.repo
+        )
+        planrepo.save()
+        change_case_template = ChangeCaseTemplate()
+        change_case_template.save()
+        build_flow.build.release = Release(
+            repo=build_flow.build.repo,
+            change_case_template=change_case_template,
+        )
+        build_flow.build.release.version_number = "1.0"
+        build_flow.build.release.save()
+        # step = DefaultImplementationStep(**build_flow.build.repo.default_implementation_steps[0])
+        # start = step.start(build_flow.build.release)
+        # push_time = step._push_time(start)
+        options = build_flow._get_flow_options()
+        assert options["push_sandbox"]["version"] == "1.0"
+        assert options["push_sandbox"]["start_time"] == timezone.make_aware(
+            datetime.datetime.combine(datetime.datetime.now().date(), datetime.time(20))
+        )
 
     def test_get_flow_options_push_sandbox_exception(self):
         with pytest.raises(Exception):
@@ -209,7 +250,9 @@ class TestBuildFlow:
 
     def test_get_flow_options_push_production(self):
         build_flow = BuildFlowFactory()
-        build_flow.build.plan = PlanFactory(role="push_production", change_traffic_control=True)
+        build_flow.build.plan = PlanFactory(
+            role="push_production", change_traffic_control=True
+        )
         build_flow.build.plan.save()
         build_flow.build.repo = RepositoryFactory(
             default_implementation_steps=[
@@ -223,7 +266,9 @@ class TestBuildFlow:
             ],
         )
         build_flow.build.repo.save()
-        planrepo = PlanRepositoryFactory(plan=build_flow.build.plan, repo=build_flow.build.repo)
+        planrepo = PlanRepositoryFactory(
+            plan=build_flow.build.plan, repo=build_flow.build.repo
+        )
         planrepo.save()
         change_case_template = ChangeCaseTemplate()
         change_case_template.save()
@@ -234,12 +279,10 @@ class TestBuildFlow:
         build_flow.build.release.version_number = "1.0"
         build_flow.build.release.save()
         options = build_flow._get_flow_options()
-        step = DefaultImplementationStep(**build_flow.build.repo.default_implementation_steps[0])
-        start = step.start(build_flow.build.release)
-        push_time = step._push_time(start)
-        options = build_flow._get_flow_options()
         assert options["push_all"]["version"] == "1.0"
-        assert options["push_all"]["start_time"] == push_time
+        assert options["push_all"]["start_time"] == timezone.make_aware(
+            datetime.datetime.combine(datetime.datetime.now().date(), datetime.time(21))
+        )
 
     def test_get_flow_options_push_production_exception(self):
         with pytest.raises(Exception):
