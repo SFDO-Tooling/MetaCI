6febcca328cdad659c67a8a24c6b37ff999e021c
Brandon Parker <brandon.parker@salesforce.com>
2021-09-16 20:50:30 -0700

    views for cohort list/view

--- metaci/release/views.py
+++ metaci/release/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
-from metaci.release.models import ReleaseCohort
+from metaci.release.models import ReleaseCohort, Release
 
 @login_required
 def cohort_list(request):
+    # TODO: check if user has view cohort perms
     cohorts = ReleaseCohort.objects.all()
     context = {'cohorts': cohorts}
     return render(request, "cohort/list.html", context=context)
 
 @login_required
 def cohort_detail(request, cohort_id):
+    # TODO: check if user has view cohort perms
     cohort = get_object_or_404(ReleaseCohort, id=cohort_id)
-    context = {'cohort': cohort}
+    releases = list(Release.objects.filter(release_cohort=cohort.id))
+    print(f'>>> releases: {releases}')
+    print(f'release repo: {releases[0].repo}')
+    print(f'release version num: {releases[0].version_number}')
+    print(f'>>> num releases: {len(releases)}')
+    print(f'>>> cohort: {cohort}')
+    context = {'cohort': cohort, 'releases': releases}
     return render(request, "cohort/detail.html", context=context)
