d30eecc9e02d6891396aae91361943468edaa437
Brandon Parker <brandon.parker@salesforce.com>
2021-09-22 20:02:22 -0700

    Release Cohort Views (#2631)
    New cohort list and detail views at `/cohorts` and `/cohorts/{cohort.id}` respectively. These views are only accessible when users are logged in _and_ they have the 'view release cohorts' permission.
    
    ## Misc.
    * I created a story to enable these views to be accessed somewhere in the UI (instead of direct via URL only).
    * We may want a story to optimize the list view. As the number of cohort records grows, we will likely want a way to show the "upcoming" or "active" release.
    * Thoughts on if people like the "merge freeze active" to be green when _not active_?
    * Do we want additional/different columns for products releasing on the detail view? 
    * Do we want additional/different columns for the list view?
    
    Co-authored-by: James Estevez <jestevez@salesforce.com>

---
+++ metaci/release/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import re_path
+
+from metaci.release import views
+
+urlpatterns = [
+    re_path(
+        r"^$",
+        views.cohort_list,
+        name="cohort_list"
+    ),
+    re_path(
+        r"^(?P<cohort_id>\w+)$",
+        views.cohort_detail,
+        name="cohort_detail"
+    ),
+]
