57576fc4aff3aab2ff2fd1296a1c52681cf2aee3
Robert Wen <cwen@salesforce.com>
2025-05-08 14:24:51 -0700

    Update chrome-smoke-test.py
    To address
    "Unable to open chrome:
    Message: session not created: probably user data directory is already in use, please specify a unique value for --user-data-dir argument, or don't use --user-data-dir"

--- .heroku/chrome-smoke-test.py
+++ .heroku/chrome-smoke-test.py
@@ -1,8 +1,10 @@
 import selenium
 import selenium.webdriver
-import sys
+import sys,os
 options = selenium.webdriver.chrome.options.Options()
 options.add_argument('--no-sandbox')
+os.mkdir('/tmp/test-data')
+options.add_argument('--user-data-dir=/tmp/test-data')
 
 try:
     selenium.webdriver.Chrome(options=options)
